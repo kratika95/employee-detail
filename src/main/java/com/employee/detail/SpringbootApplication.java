a97adb1101e3a45f6ba85d51cebc1c55fb24f291
Kratika Sonker <hp@DESKTOP-T53J3VM.mshome.net>
2020-10-03 20:51:42 +0530

    First commit with basic files rrequired for spring boot application.

---
+++ src/main/java/com/employee/detail/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.employee.detail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/employee/detail/SpringbootApplicationTest.java
@@ -0,0 +1,14 @@
+package com.employee.detail;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpringbootApplicationTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
